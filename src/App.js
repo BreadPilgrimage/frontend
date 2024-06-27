0c79292eee946193a2cbb2869edc759fb83c0c30
Yejin <kyj5840kyj@duksung.ac.kr>
2024-06-27 18:08:03 +0900

    #1 Feat: 프로젝트 생성 및 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MapPage from "./pages/MapPage";
+import Header from "./components/layout/Header";
+import Navbar from "./components/layout/Navbar";
+import MainPage from "./pages/MainPage";
+import BreadPage from "./pages/BreadPage";
+import SearchPage from "./pages/SearchPage";
+import ShopPage from "./pages/ShopPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/map" element={<MapPage />} />
+            <Route path="/breadpage" element={<BreadPage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/shop" element={<ShopPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      <Navbar />
+    </>
   );
 }
 
